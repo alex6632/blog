a17dc2b5af5b797438a8f52f9da36dc497de22ec
Alexandre <a.simonin69@gmail.com>
2016-11-26 18:12:52 +0100

    update gitignore

--- includes/functions.php
+++ includes/functions.php
@@ -1,18 +1,5 @@
 <?php
 
-function bdd_connect() {
-
-    global $link;
-
-    $link = mysqli_connect('localhost', 'root', '', 'blog');
-
-    $link->set_charset("utf8");
-
-    if (!$link) {
-        die('Erreur de connexion (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
-    }
-}
-
 function execute_query($query) {
 
     global $link;
