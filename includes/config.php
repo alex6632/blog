f96a37030aa06314cbad1c56067095c24b12f1f0
Alexandre <a.simonin69@gmail.com>
2016-11-26 18:02:44 +0100

    Add config

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+function bdd_connect() {
+
+    global $link;
+
+    $link = mysqli_connect('localhost', 'root', '', 'blog');
+
+    $link->set_charset("utf8");
+
+    if (!$link) {
+        die('Erreur de connexion (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+    }
+}
